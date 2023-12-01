f36d3fc64be47532a28364052b08cdebf8d9c3e8
Zamavundla <zsdouzee8@gmail.com>
2023-12-01 08:23:07 +0200

    Genre code JS documenting

--- Podcast/src/components/Card.jsx
+++ Podcast/src/components/Card.jsx
@@ -9,12 +9,27 @@
  * @param {string} props.genre - The genre(s) of the TV show or movie.
  * @param {string} props.lastUpdated - The date when the information was last updated.
  * @param {Function} props.handleShow - The function to be called when the card is clicked.
+ * @param {number} [props.selectedGenre] - The selected genre number to filter cards (optional).
  * @returns {JSX.Element} The JSX representation of the Card component.
  * 
+ * @example
+ * // Example usage of Card component
+ * <Card
+ *   title="Podcast Title"
+ *   image="https://example.com/podcast-image.jpg"
+ *   season={5}
+ *   genre="Business"
+ *   lastUpdated="2023-11-30"
+ *   handleShow={handleCardClick}
+ * />
  */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
+/**
+ * Object containing genre labels based on genre numbers.
+ * @type {Object.<string, string>}
+ */
 const genres = {
   1: 'Personal Growth',
   2: 'True Crime and Investigative Journalism',
@@ -27,7 +42,23 @@ const genres = {
   9: 'Kids and Family',
 };
 
+/**
+ * Functional component representing a Card displaying podcast information.
+ * @param {Object} props - Props for the Card component.
+ * @param {string} props.title - Title of the podcast.
+ * @param {string} props.image - URL of the podcast image.
+ * @param {number} props.season - Number of seasons.
+ * @param {string} props.genre - Genre(s) of the podcast.
+ * @param {string} props.lastUpdated - Last updated date of the podcast information.
+ * @param {Function} props.handleShow - Function to handle card click.
+ * @param {number} [props.selectedGenre] - Selected genre number (optional).
+ * @returns {JSX.Element} JSX representation of the Card component.
+ */
 const Card = ({ title, image, season, genre, lastUpdated, handleShow, selectedGenre }) => {
+  /**
+   * Determines whether to display the card based on the selected genre.
+   * @type {boolean}
+   */
   const shouldDisplay = !selectedGenre || genres[selectedGenre] === genre;
 
   return shouldDisplay ? (
